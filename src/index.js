1c20de5dad96838ec8250c7eae2b8e8775490420
Tim Leverett <tleverett@nexusmediainc.net>
2016-11-03 15:25:17 -0400

    fix tyop

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const less = require('gulp-less');
 const path = require('path');
 const rename = require('gulp-rename');
 const watch = require('gulp-watch');
-const yargs = requrie('yargs');
+const yargs = require('yargs');
 
 class LESSBuild {
     constructor(src, dest, gulp) {
